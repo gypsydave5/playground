5251cf8e260130d4e187be0b715dc7101a2d9c43
David Wickes <dave.wickes@gmail.com>
2016-03-05 13:09:39 +0000

    GoPL - dup

---
+++ gopl/dup/dup2.go
@@ -0,0 +1,75 @@
+// Dup2 prints the count and text of lines that appear more than once in the
+// input. It reads from stdin or from a list of named files.
+//
+// It also now records which files were written to
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	linesInFiles := make(map[string][]string)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLinesAndNoteFiles(f, counts, arg, linesInFiles)
+			f.Close()
+		}
+	}
+	printLinesRepeating(2, counts, linesInFiles)
+}
+
+func printLinesRepeating(
+	threshold int,
+	counts map[string]int,
+	linesInFiles map[string][]string,
+) {
+	for line, n := range counts {
+		if n >= threshold {
+			fmt.Printf("%q\t%d\t%s\n", strings.Join(linesInFiles[line], ", "), n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
+
+func countLinesAndNoteFiles(
+	f *os.File,
+	counts map[string]int,
+	arg string,
+	linesInFiles map[string][]string,
+) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+		if !contains(linesInFiles[input.Text()], arg) {
+			linesInFiles[input.Text()] = append(linesInFiles[input.Text()], arg)
+		}
+	}
+}
+
+func contains(slice []string, str string) bool {
+	for _, s := range slice {
+		if s == str {
+			return true
+		}
+	}
+	return false
+}
