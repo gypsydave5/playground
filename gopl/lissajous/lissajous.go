6463b7fd72071035ea74dc18d26ae848d3764b68
David Wickes <dave.wickes@gmail.com>
2016-03-09 08:29:51 +0000

    Flashy colours

--- gopl/lissajous/lissajous.go
+++ gopl/lissajous/lissajous.go
@@ -10,11 +10,16 @@ import (
 	"os"
 )
 
-var palette = []color.Color{color.Black, color.RGBA{0x00, 0xff, 0x00, 0xff}}
+var palette = []color.Color{
+	color.Black,
+	color.RGBA{0x00, 0xff, 0x00, 0xff},
+	color.RGBA{0xff, 0x00, 0x00, 0xff},
+}
 
 const (
-	whiteIndex = 0
-	blackIndex = 1
+	blackIndex = 0
+	greenIndex = 1
+	redIndex   = 2
 )
 
 func main() {
@@ -33,12 +38,18 @@ func lissajous(out io.Writer) {
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 //phase difference
 	for i := 0; i < nframes; i++ {
+		var colorIndex uint8
+		if i%2 == 0 {
+			colorIndex = greenIndex
+		} else {
+			colorIndex = redIndex
+		}
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), colorIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
