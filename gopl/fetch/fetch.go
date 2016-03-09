2f8c5c537534fba1b51e43017e80b155c7c8513f
David Wickes <dave.wickes@gmail.com>
2016-03-09 23:05:03 +0000

    some fun tests here

--- gopl/fetch/fetch.go
+++ gopl/fetch/fetch.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	for _, url := range os.Args[1:] {
 		url = addSchema(url)
-		resp, err := http.Get(url)
+		resp, err := fetch(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
@@ -24,6 +24,23 @@ func main() {
 			os.Exit(1)
 		}
 	}
+}
+
+func fetch(url string) (*http.Response, error) {
+	return http.Get(url)
+}
+
+func fetchMany(urls []string) ([]*http.Response, error) {
+	var resps []*http.Response
+	var err error
+	for _, url := range urls {
+		resp, err := fetch(url)
+		if err != nil {
+			return resps, err
+		}
+		resps = append(resps, resp)
+	}
+	return resps, err
 }
 
 func addSchema(url string) string {
--- gopl/fetch/fetch_test.go
+++ gopl/fetch/fetch_test.go
@@ -1,6 +1,13 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+)
 
 func TestPrefixAdderHttp(t *testing.T) {
 	url := "http://bob.com"
@@ -23,5 +30,51 @@ func TestAddSchemaNoSchema(t *testing.T) {
 	expectation := "http://bob.com"
 	if result != expectation {
 		t.Errorf(`expected %q, got %q`, expectation, result)
+	}
+}
+
+func TestMakingGoodHTTP(t *testing.T) {
+	expectation := "Hello, delicious friend"
+	ts := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, expectation)
+		}))
+	defer ts.Close()
+
+	resp, _ := fetch(ts.URL)
+	b, _ := ioutil.ReadAll(resp.Body)
+
+	resp.Body.Close()
+	if string(b) != expectation {
+		t.Errorf(`expected %q, got %q`, expectation, string(b))
+	}
+}
+
+func TestMakingBadHTTP(t *testing.T) {
+	_, err := fetch("http://badbadurl")
+	if err == nil {
+		t.Errorf(`expected fetch to error when there's no server`)
+	}
+}
+
+func TestFetchingMany(t *testing.T) {
+	callCount := 0
+	ts := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			callCount++
+			responseString := "Call number " + strconv.Itoa(callCount)
+			fmt.Fprint(w, responseString)
+		}))
+	defer ts.Close()
+
+	urls := []string{ts.URL + "/first", ts.URL + "/second"}
+	resps, _ := fetchMany(urls)
+	b1, _ := ioutil.ReadAll(resps[0].Body)
+	b2, _ := ioutil.ReadAll(resps[1].Body)
+	if string(b1) != "Call number 1" {
+		t.Errorf(`expected %q, got %q`, "Call number 1", string(b1))
+	}
+	if string(b2) != "Call number 2" {
+		t.Errorf(`expected %q, got %q`, "Call number 2", string(b2))
 	}
 }
