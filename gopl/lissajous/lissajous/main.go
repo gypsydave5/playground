bc3f40b843274c43356c85a6826032a16ee393da
David Wickes <dave.wickes@gmail.com>
2016-03-19 22:35:43 +0000

    Lissajous command line options

--- gopl/lissajous/lissajous/main.go
+++ gopl/lissajous/lissajous/main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
-	"github.com/gypsydave5/playground/gopl/lissajous"
+	"flag"
+	"log"
+	"math/rand"
 	"os"
+
+	"github.com/gypsydave5/playground/gopl/lissajous"
 )
 
+var cycles = flag.Float64("cycles", 5.0, "number of complete x oscillator revolutions")
+var resolution = flag.Float64("resolution", 0.001, "angular resolution")
+var size = flag.Int("size", 100, "image canvas covers [-size..+size]")
+var frames = flag.Int("frames", 64, "number of animation frames")
+var delay = flag.Int("delay", 8, "delay between frames in 10ms units")
+var frequency = flag.Float64("frequency", rand.Float64()*3, "relative frequency of y oscillator")
+var outputFile = flag.String("out", "", "name of file to output GIF to")
+
 func main() {
+	flag.Parse()
 	opts := lissajous.Options{
-		Cycles:     5,
-		Resolution: 0.001,
-		Size:       100,
-		Frames:     64,
-		Delay:      8,
+		Cycles:     *cycles,
+		Resolution: *resolution,
+		Size:       *size,
+		Frames:     *frames,
+		Delay:      *delay,
+		Frequency:  *frequency,
 	}
-	lissajous.Lissajous(os.Stdout, opts)
+
+	if *outputFile == "" {
+		lissajous.Lissajous(os.Stdout, opts)
+	} else {
+		f, err := os.Create(*outputFile)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			lissajous.Lissajous(f, opts)
+		}
+	}
 }
