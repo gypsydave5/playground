87efc8753896fb37ebef20e0bd7c331130ad5e8b
David Wickes <dave.wickes@gmail.com>
2016-05-09 19:07:24 +0100

    Playing around with the test some more

--- gopl/chapter1/fetchall/main_test.go
+++ gopl/chapter1/fetchall/main_test.go
@@ -10,28 +10,35 @@ import (
 )
 
 func TestFetchConcurrent(t *testing.T) {
-	ts := httptest.NewServer(
+	ts1 := httptest.NewServer(
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintln(w, "hello!")
 			time.Sleep(100 * time.Millisecond)
 		}))
+
+	ts2 := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintln(w, "hi")
+			time.Sleep(200 * time.Millisecond)
+		}))
 	ch := make(chan string, 2)
 
-	go fetchConcurrent(ts.URL, ch)
-	go fetchConcurrent(ts.URL, ch)
+	go fetchConcurrent(ts1.URL, ch)
+	go fetchConcurrent(ts2.URL, ch)
 
-	validReportRegex := fmt.Sprintf("^0\\.[0-9]{2}s\\s+%d\\s%s", 7, regexp.QuoteMeta(ts.URL))
-	fmt.Println(validReportRegex)
-	reportRegex, _ := regexp.Compile(validReportRegex)
+	validReportRegexOne := fmt.Sprintf("^0\\.1[0-9]{1}s\\s+%d\\s%s", 7, regexp.QuoteMeta(ts1.URL))
+	validReportRegexTwo := fmt.Sprintf("^0\\.2[0-9]{1}s\\s+%d\\s%s", 3, regexp.QuoteMeta(ts2.URL))
+	reportRegexOne, _ := regexp.Compile(validReportRegexOne)
+	reportRegexTwo, _ := regexp.Compile(validReportRegexTwo)
 
 	out := <-ch
 	out2 := <-ch
 
-	if !reportRegex.MatchString(out) {
+	if !reportRegexOne.MatchString(out) {
 		t.Errorf("invalid report string, %v", out)
 	}
 
-	if !reportRegex.MatchString(out2) {
+	if !reportRegexTwo.MatchString(out2) {
 		t.Errorf("invalid report string, %v", out2)
 	}
 }
