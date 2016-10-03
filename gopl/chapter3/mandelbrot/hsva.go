6568e1e007be7038d59e9334b5e2cacdc21daf07
David Wickes <dave.wickes@gmail.com>
2016-10-03 16:24:11 +0100

    refactor of hsva

--- gopl/chapter3/mandelbrot/hsva.go
+++ gopl/chapter3/mandelbrot/hsva.go
@@ -14,49 +14,38 @@ type HSVA struct {
 
 // RGBA returns the HSVA color as RGBA values as per the color.Color interface
 func (h HSVA) RGBA() (r uint32, g uint32, b uint32, a uint32) {
-	var rf, gf, bf, af float64
+	var rf, gf, bf float64
+	hp := h.H / 60.0
 
 	c := h.S * h.V
-	x := c * (1 - math.Abs(math.Mod(h.H/60, 2.0)-1.0))
+	x := c * (1.0 - math.Abs(math.Mod(hp, 2.0)-1.0))
 	m := h.V - c
 
 	switch {
 	case h.H < 60:
 		rf = c
 		gf = x
-		bf = 0
-		af = h.A
 	case h.H < 120:
 		rf = x
 		gf = c
-		bf = 0
-		af = h.A
 	case h.H < 180:
-		rf = 0
 		gf = c
 		bf = x
-		af = h.A
 	case h.H < 240:
-		rf = 0
 		gf = x
 		bf = c
-		af = h.A
 	case h.H < 300:
 		rf = x
-		gf = 0
 		bf = c
-		af = h.A
 	case h.H < 360:
 		rf = c
-		gf = 0
 		bf = x
-		af = h.A
 	}
 
-	r = uint32((rf + m) * 0xffff)
-	g = uint32((gf + m) * 0xffff)
-	b = uint32((bf + m) * 0xffff)
-	a = uint32(af * 0xffff)
+	r = uint32((rf + m) * 65535.0)
+	g = uint32((gf + m) * 65535.0)
+	b = uint32((bf + m) * 65535.0)
+	a = uint32(h.A * 65535.0)
 
 	return r, g, b, a
 }
