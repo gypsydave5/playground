92fd7c17533588609cf3a98b95d89ba3cee3f406
David Wickes <dave.wickes@gmail.com>
2019-04-29 23:45:27 +0100

    Um... please do not look at the test last time... It was late...

--- gopl/chapter3/comma/comma.go
+++ gopl/chapter3/comma/comma.go
@@ -1,15 +1,36 @@
 package main
 
-import "bytes"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(comma("123456.123456"))
+}
 
 // Exercise 3.10
 func comma(s string) string {
 	var result bytes.Buffer
+
+	if s[0] == '+' || s[0] == '-' {
+		result.WriteRune(rune(s[0]))
+		s = s[1:]
+	}
+
+	split := strings.SplitN(s, ".", 2)
+	s = split[0]
+
 	for i, r := range s {
-		if i+(len(s))%3 == 0 {
+		result.WriteRune(r)
+		if (len(s)-i-1)%3 == 0 && i != len(s)-1 {
 			result.WriteRune(',')
 		}
-		result.WriteRune(r)
+	}
+	if len(split) == 2 {
+		result.WriteRune('.')
+		result.WriteString(split[1])
 	}
 	return result.String()
 }
--- gopl/chapter3/comma/comma_test.go
+++ gopl/chapter3/comma/comma_test.go
@@ -11,11 +11,14 @@ func TestComma(t *testing.T) {
 		{"10", "10"},
 		{"9999", "9,999"},
 		{"123123123", "123,123,123"},
+		{"23123123", "23,123,123"},
+		{"+123123123", "+123,123,123"},
+		{"+12311.23123456", "+12,311.23123456"},
 	}
 
 	for _, c := range cases {
 		t.Run(c.input+"->"+c.want, func(t *testing.T) {
-			got := comma(c.want)
+			got := comma(c.input)
 			if got != c.want {
 				t.Errorf("Got %#v, wanted %#v", got, c.want)
 			}
